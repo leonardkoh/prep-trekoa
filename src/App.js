53e1ed2ac60ae0199045a1db451f01aaf6644086
lkadmin <lkhome@lkadmin.home>
2020-07-26 18:18:23 +1000

    classes added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,15 @@
 import React from 'react';
 import './App.css';
-// import { render } from '@testing-library/react';
+
+const items = {
+  "Rucksack": false,
+  "Tent": false,
+  "Food": false,
+  "Sleeping Bag": false,
+  "Sleeping Mat": false,
+  "Rain Jacket": false,
+  "Water bottles": false,
+};
 
 class Header extends React.Component {
   render() {
@@ -13,7 +22,7 @@ class Header extends React.Component {
           </div>
         <div className="col pr-5 text-right">
           {/* add onclick handler */}
-            <img className="header-logo" src="reset.svg" alt="reset"></img>
+            <img className="header-button mt-3" src="reset.svg" alt="reset"></img>
         </div>
 
       </div>
@@ -21,11 +30,47 @@ class Header extends React.Component {
   }
 }
 
+class Category extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+  render() {
+    return ( 
+    <div>
+        <h3 className="row pl-3 pb-2">{this.props.name}</h3>
+    </div>
+    )}
+
+} 
+
+class Item extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+  render() {
+    return ( 
+    <div>
+      
+    </div>
+    )}
+}
+
 function App() {
   return (
-    <div>
+    <div className="container-fluid">
       <Header />
-      hello
+      <section className="row">
+        <h2 className="col py-3 pl-3">Have you packed everything?</h2>
+      </section>
+      <section>
+        <Category name="Essentials" />
+        <div className="row">
+          <div className="col text-center">Rucksack</div>
+          <div className="col text-center">Sleeping Mat</div>
+          <div className="col text-center">Sleeping Bag</div>
+          <div className="col text-center">Rucksack</div>
+        </div>
+      </section>
     </div>
   );
 }
