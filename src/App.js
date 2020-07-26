8eb15978a70b4d3c72d7978ed90680c9177f5f3f
lkadmin <lkhome@lkadmin.local>
2020-07-26 15:49:43 +1000

    update logo and styling

--- src/App.js
+++ src/App.js
@@ -1,10 +1,31 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+// import { render } from '@testing-library/react';
+
+class Header extends React.Component {
+  render() {
+    return (
+      <div className="header-style row py-3">
+          <div className="col pl-5">
+          <a href="http://www.trekoa.com">
+            <img className="header-logo" src="trekoa-logo.png" alt="trekoa-logo"></img>
+          </a>
+          </div>
+        <div className="col pr-5 text-right">
+          {/* onclick */}
+            <img className="header-logo" src="reset.svg" alt="reset"></img>
+        </div>
+
+      </div>
+    )
+  }
+}
 
 function App() {
   return (
-    <div className="App">
+    <div>
+      <Header />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
