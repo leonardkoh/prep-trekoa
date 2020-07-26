c2b2392e92075c8511a1373ef91749bbec0dcc9a
lkadmin <lkhome@lkadmin.home>
2020-07-26 23:13:42 +1000

    add info icon and logic for image switch

--- src/App.js
+++ src/App.js
@@ -3,48 +3,48 @@ import './App.css';
 
 const Essentials = {
   "Rucksack": true,
-  "Sleeping Bag": false,
+  "Sleeping Bag": true,
   "Sleeping Mat": false,
   "Cutlery": false,
   "Plate/Bowl": true,
   "Mug": false,
+  "Water bottles": false,
   "Sunscreen": false,
   "Insect Repellant": false,
 };
 
-// const Clothing = {
-//   "Shoes/Boots": false,
-//   "Camp Shoes": false,
-//   "Socks": false,
-//   "Hiking pants/shorts": false,
-//   "Thermals": false,
-//   "Hat": false,
-//   "Beanie": false,
-//   "Sunglasses": false,
-//   "Mid layer / Fleece top": false,
-//   "Long/Short sleeve shirt": false,
-//   "Rain Jacket": false,
-// };
+const Clothing = {
+  "Shoes/Boots": false,
+  "Camp Shoes": false,
+  "Socks": false,
+  "Hiking pants/Shorts": false,
+  "Thermals": false,
+  "Hat": false,
+  "Beanie": false,
+  "Sunglasses": false,
+  "Mid layer/Fleece top": false,
+  "Long/Short sleeve shirt": false,
+  "Rain Jacket": false,
+};
 
-// const Food = {
-//   "Water bottles": false,
-//   "Breakfast": false,
-//   "Lunch": false,
-//   "Dinner": false,
-// };
+const Food = {
+  "Breakfast": false,
+  "Lunch": false,
+  "Dinner": false,
+};
 
-// const Extras = {
-//   "Tent": false,
-//   "Stove": false,
-//   "Fuel": false,
-//   "Lighter/Matches": false,
-//   "Pots/Pans": false,
-//   "First Aid Kit": false,
-//   "Headlamp": false,
-//   "Multi Tool / Knife": false,
-//   "Pack Liner/Garbage Bag": false,
-//   "Phone/Watch": false,
-// };
+const Extras = {
+  "Tent": false,
+  "Stove": false,
+  "Fuel": false,
+  "Lighter/Matches": false,
+  "Pots/Pans": false,
+  "First Aid Kit": false,
+  "Headlamp": false,
+  "Multi Tool/Knife": false,
+  "Pack Liner/Garbage Bag": false,
+  "Phone/Watch": false,
+};
 
 class Header extends React.Component {
   render() {
@@ -73,21 +73,34 @@ class Category extends React.Component {
   }
 
   handleImageClick() {
+    //toggle
     console.log("Inside HandleImageClick");
   }
 
   render() {
+    let category = {};
+    switch (this.props.name) {
+      case "Essentials": 
+        category = Essentials; break;
+      case "Clothing": 
+        category = Clothing; break;
+      case "Food": 
+        category = Food; break;
+      default: 
+        category = Extras; break;
+    };
+
     return ( 
     <section className="py-2">
         <h2 className="row pl-3 pb-1 font-weight-bold">{this.props.name}</h2>
-        { Object.keys(Essentials).map((k,v) => {
+        { Object.keys(category).map((k,v) => {
           return <div className="row text-left" key={`${k}-${v}`}>
-            <p className="col pl-3">{k}</p>
-            {k.v ?
-            <img className="col" src="checked.svg" alt="tick-check" onClick={this.handleImageClick}></img> :
-            <img className="col" src="unchecked.svg" alt="untick-check" onClick={this.handleImageClick}></img> 
+            <p className="col-8 pl-3 pt-1">{k}</p>
+            { category[k] ? 
+              <img className="col-2" src="checked.svg" alt="tick-check" onClick={this.handleImageClick}></img> :
+              <img className="col-2" src="unchecked.svg" alt="untick-check" onClick={this.handleImageClick}></img> 
             }
-            <div className="col"></div>
+            <img className="col-2" src="more-info.svg" alt="more-info" onClick={this.handleImageClick}></img>
           </div>;
         })
 
@@ -97,24 +110,24 @@ class Category extends React.Component {
 
 } 
 
-// class Item extends React.Component {
-//   constructor(props) {
-//     super(props);
-//   }
-//   render() {
-//     return ( 
-//     <div>
+class Item extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return ( 
+    <div>
       
-//     </div>
-//     )}
-// }
+    </div>
+    )}
+}
 
 function App() {
   return (
     <div className="container-fluid">
       <Header />
       <section className="row">
-        <h1 className="col py-3 text-center font-weight-bold">Have you packed everything?</h1>
+        <h1 className="col py-3 text-center font-weight-bold">Duke of Ed Pack List</h1>
       </section>
         <Category name="Essentials" />
         <Category name="Clothing" />
