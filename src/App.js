78269b2ad24886524e087883cb4f42c7c27572b2
LeonardK <40259905+leonardkoh@users.noreply.github.com>
2020-07-29 23:55:41 +1000

    calculator and optional item color coding added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.css';
+import { render } from '@testing-library/react';
 
 const ICON_PATH = "images/items/"
 
@@ -159,6 +160,23 @@ const MISC_ICONS = {
   "Compass": ICON_PATH + "compass.svg",
 };
 
+const OPTIONAL_ITEMS = {
+  "Insect repellant": true,
+  "Thermals": true,
+  "Camp shoes": true,
+  "Beanie": true,
+  "Tent": true,
+  "Stove": true,
+  "Fuel": true,
+  "Lighter/Matches": false,
+  "Pots/Pans": false,
+  "Compass": false,
+  "First aid kit": false,
+  "Multi Tool/Knife": false,
+  "Toiletries": false,
+  "Small towel": false,
+}
+
 function AppInfo() {
   return (
     <div>
@@ -172,12 +190,11 @@ function AppInfo() {
 
 function WeightInfo() {
   return (
-    <div>
-      <h4 className="text-center">What do the coloured items mean?</h4>
+    <div className="pt-1">
+      <h4 className="text-center pb-1">What do the coloured items mean?</h4>
         <h5>Glad you asked! Experts suggest you should carry less than 20% of your body weight.</h5>
-        <h5>Consider removing/splitting some of the these items</h5>
-        <h5>Here's a little tool to help you out :)</h5>
-    </div>
+        <h5 className="pb-1">Consider removing/splitting some of the these items</h5>
+    </div>    
   )
 }
 
@@ -276,23 +293,49 @@ class Item extends React.Component {
     return ( 
       <div className="row text-left" key={`${itemKey}-${itemCategory}`}>
       <img src={itemIcon} className="col-2"></img>
-      <p className="col-6 pl-3 pt-1">{itemKey}</p>
-        { isChecked ? 
-          <input type="image" className="col-2 tickbox-circle" src="images/checked.svg" alt="tick-check" onClick={this.toggleChecked}></input> :
-          <input type="image" className="col-2 tickbox-circle" src="images/unchecked.svg" alt="untick-check" onClick={this.toggleChecked}></input>
-        }
-        <input type="image" className="col-2 tickbox-circle" value={showInfo} src="images/more-info.svg" alt="more-info" onClick={this.toggleMoreInfo}></input>
-        { showInfo ? 
-          <div className="info-text pb-3 px-3">{this.props.itemInfo}</div>: 
-          <p></p> 
-        }
-      
+      {
+         itemKey in OPTIONAL_ITEMS ? <p className="col-6 pl-3 pt-1 optional-item">{itemKey}</p> : <p className="col-6 pl-3 pt-1">{itemKey}</p>
+      }
+      { isChecked ? 
+        <input type="image" className="col-2 tickbox-circle" src="images/checked.svg" alt="tick-check" onClick={this.toggleChecked}></input> :
+        <input type="image" className="col-2 tickbox-circle" src="images/unchecked.svg" alt="untick-check" onClick={this.toggleChecked}></input>
+      }
+      <input type="image" className="col-2 tickbox-circle" value={showInfo} src="images/more-info.svg" alt="more-info" onClick={this.toggleMoreInfo}></input>
+      { showInfo ? 
+        <div className="info-text pb-3 px-3">{this.props.itemInfo}</div>: 
+        <p></p> 
+      }
     </div>
     )}
 }
 
 class Calculator extends React.Component {
-  
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      weight: ""
+    }
+
+    this.handleChange = this.handleChange.bind(this);
+  }
+  handleChange(e) {
+    this.setState({weight: e.target.value});
+  }
+
+  render() {
+    return (
+      <div className="row px-2 text-center">
+        <div className="col-6">
+        <h5>My weight</h5>
+        <h5><input className="text-center" type="text" placeholder="kg" value={this.state.weight} onChange={this.handleChange}></input></h5>
+        </div>
+        <div className="col-6">
+          <h5>Pack weight</h5>
+          <h5><input className="text-center" type="text" value={(this.state.weight*0.2).toFixed(1)} readOnly></input></h5>
+        </div>
+      </div>
+    )}
 }
 
 function App() {
