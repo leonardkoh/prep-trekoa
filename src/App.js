d5281ed9aa836f934d384302f0b6e7a80e50785d
LeonardK <40259905+leonardkoh@users.noreply.github.com>
2020-07-30 13:53:32 +1000

    optional items modified

--- src/App.js
+++ src/App.js
@@ -161,6 +161,7 @@ const MISC_ICONS = {
 
 const OPTIONAL_ITEMS = {
   "Insect repellant": true,
+  "Snacks": true,
   "Thermals": true,
   "Camp shoes": true,
   "Beanie": true,
@@ -192,7 +193,7 @@ function WeightInfo() {
     <div className="pt-1">
       <h4 className="text-center pb-1">What do the coloured items mean?</h4>
         <h5>Glad you asked! Experts suggest you should carry less than 20% of your body weight.</h5>
-        <h5 className="pb-1">Consider removing/splitting some of the these items</h5>
+        <h5 className="pb-1">Consider removing or reducing some of the these items</h5>
     </div>    
   )
 }
